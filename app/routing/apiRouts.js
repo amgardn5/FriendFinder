53ec7d73668887c41108fab664d5d1a4c013e540
Aaron <aaronmgardner1@gmail.com>
2019-08-23 22:20:31 -0700

    Basic functionality, no modal

--- app/routing/apiRouts.js
+++ app/routing/apiRouts.js
@@ -1,4 +1,8 @@
-var friend = require("../data/friends");
+var friends = require("../data/friends");
+const express = require('express');
+const path = require('path');
+
+const app = express();
 
 module.exports = (app) => {
 
@@ -12,11 +16,29 @@ module.exports = (app) => {
     // This will be used to handle incoming survey results. 
     // This route will also be used to handle the compatibility logic.
     app.post("/api/friends", (req, res) => {
+        let data = req.body;
+        var friend = findFriend(data.scores);
+
+        res.send(friends.data[friend]);
 
     });
 };
 
-
 function findFriend(data){
-
+    var friend_id = 0;
+    var max_diff = 99;
+    for(let i=0; i < friends.data.length; i++){
+        let friend = friends.data[i];
+        let diff = 0;
+        for(let j=0; j < friend.scores.length; j++){
+            diff += Math.abs(friend.scores[j] - parseInt(data[j]));
+        }
+        if (diff < max_diff){
+            console.log("new match old score="+ max_diff + " new score: "+ diff);
+            max_diff = diff;
+            friend_id = i;
+        }
+    }
+    console.log(friend_id);
+    return friend_id;
 };
--- server.js
+++ server.js
@@ -10,5 +10,5 @@ require('./app/routing/apiRouts')(app);
 require('./app/routing/htmlRouts')(app);
 
 app.listen(process.env.PORT || 8080, function() {
-    console.log(`App listening on PORT 8080`);
+    console.log(`App listening on PORT 8080. http://localhost:8080`);
 });
